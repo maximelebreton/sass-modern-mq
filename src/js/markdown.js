c9dc530d6bcfc65885b3204892704a2939825d50
Maxime Le Breton <mail@maximelebreton.com>
2020-06-10 23:16:19 +0200

    reminify

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -1,7 +1,6 @@
 import mdfile from "/README.md";
-import markdownIt from "markdown-it/dist/markdown-it.min.js";
-import markdownItHighlight from "markdown-it-highlight/dist/index.js";
-import "markdown-it-highlight/dist/index.css";
+import markdownIt from "markdown-it";
+import markdownItHighlight from "markdown-it-highlight";
 
 const mdi = markdownIt();
 mdi.use(markdownItHighlight);
